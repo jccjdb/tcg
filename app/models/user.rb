3b8b5ee8c10a5b5a9f1ace98e737f3ccc6096bdb
Jade Cribbs <jccjdb@aol.com>
2020-03-22 20:31:58 +0000

    Stupiderror

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :info
+  has_many :infos
 end
--- spec/controllers/infos_controller_spec.rb
+++ spec/controllers/infos_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe InfosController, type: :controller do
 
-  describe "info#new action" do
+  describe "infos#new action" do
     it "should require users to be logged in" do
       get :new
     expect(response).to redirect_to new_user_session_path
@@ -22,7 +22,7 @@ RSpec.describe InfosController, type: :controller do
     end
   end
 
-  describe "info#create action" do
+  describe "infos#create action" do
 
     it "should require users to be logged in" do
       post :create, params: { gram: { message: "First gen" } }
@@ -51,13 +51,12 @@ RSpec.describe InfosController, type: :controller do
         password:              'secretPassword',
         password_confirmation: 'secretPassword'
       )
-      sign_in user
-
+                           
       info_count = Info.count
       post :create, params: { info: { message: ''} }
       expect(response).to have_http_status(:unprocessable_entity)
-      expect(Info_count).to eq Info.count
+      expect(info_count).to eq Info.count
     end
+
   end
 end
-
